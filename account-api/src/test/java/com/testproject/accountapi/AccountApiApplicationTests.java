f30696ff7edd0efd2c8c5a446ccde9c5ac86f950
anyapinchuk <nutaanuta-30.10.30@mail.ru>
2020-04-22 10:08:35 +0300

    added account api, zuul gateway

---
+++ account-api/src/test/java/com/testproject/accountapi/AccountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.accountapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
